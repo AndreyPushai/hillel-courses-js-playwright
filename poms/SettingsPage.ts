a3bc2a6a14a71b849f48cbbb386e730b25c81317
Andrey Pushai <andrey.pushai@gmail.com>
2025-04-23 13:32:07 +0200

    Upd SettingsPage.ts
    - Added clickRemoveMyAccountButton method

--- poms/SettingsPage.ts
+++ poms/SettingsPage.ts
@@ -2,4 +2,8 @@ import BasePage from "./BasePage";
 
 export default class SettingsPage extends BasePage {
     public removeMyAccountButton = this.page.locator("button", {hasText: "Remove my account"});
+
+    async clickRemoveMyAccountButton(): Promise<void> {
+        await this.removeMyAccountButton.click();
+    };
 };
