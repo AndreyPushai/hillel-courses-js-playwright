1f1a2426a41f0205e898a640b1d84926b0763eb4
Andrey Pushai <andrey.pushai@gmail.com>
2025-05-19 18:12:46 +0200

    Upd fixtures

--- fixtures/fixtures.ts
+++ fixtures/fixtures.ts
@@ -1,13 +1,14 @@
-import { test as base } from "@playwright/test";
+import { test as base, APIRequestContext } from "@playwright/test";
 import { GaragePage } from "../poms";
 
 type MyFixtures = {
-    userGaragePage: GaragePage
+    userGaragePage: GaragePage,
+    userRequest: APIRequestContext
 };
 
 export const test = base.extend<MyFixtures>({
     userGaragePage: async ({ browser, baseURL }, use) => {
-        const context = await browser.newContext({ storageState: "utils/user.json"});
+        const context = await browser.newContext({ storageState: "utils/user.json" });
         const page = await context.newPage();
         const garagePage = new GaragePage(page);
         await page.goto(baseURL);
@@ -15,6 +16,19 @@ export const test = base.extend<MyFixtures>({
         await use(garagePage);
 
         await context.close();
+    },
+    // eslint-disable-next-line
+    userRequest: async ({ baseURL, request }, use) => {
+        await request.post(`${baseURL}/api/auth/signin`, {
+            data: {
+                "email": process.env.userEmail2,
+                "password": process.env.userPassword2,
+                "remember": false
+            }
+        });
+
+        await request.storageState({ path: "utils/user2.json" });
+        await use(request);
     }
 });
 export { expect } from '@playwright/test';
