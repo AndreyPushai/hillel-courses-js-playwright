f916cb49c0d52da7c22628d96edb2f160637741c
Andrey Pushai <andrey.pushai@gmail.com>
2025-05-20 17:39:14 +0200

    Upd profile.spec.ts
    - Added test with mocked user profile data

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -23,4 +23,26 @@ test.describe("Test profile name can be changed", () => {
         await expect(profilePage.profileName).toContainText(name);
         await expect(profilePage.profileName).toContainText(lastName);
     });
+
+    test("Mock user data", async ({ userGaragePage }) => {
+
+        const name = faker.person.firstName();
+        const lastName = faker.person.lastName();
+
+        await userGaragePage.page.route('/api/users/profile', async route => {
+            const response = await route.fetch();
+            const json = await response.json();
+            
+            json.data.name = name;
+            json.data.lastName = lastName;
+
+            await route.fulfill({ response, json });
+        });
+        await userGaragePage.page.goto("/panel/profile");
+
+
+        profilePage = new ProfilePage(userGaragePage.page); 
+        await expect(profilePage.profileName).toContainText(name);
+        await expect(profilePage.profileName).toContainText(lastName);
+    });
 });
